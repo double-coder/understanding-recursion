cb187ca1321a03dd89a5cb7a74fe15142c658cb1
Aditya Pathania <getaddi1711@gmail.com>
2022-08-18 05:16:04 +0530

    count-fix

--- count.go
+++ count.go
@@ -3,8 +3,8 @@ package main
 func count(n int) int {
 	// base case = stop recursion (we can multiple base cases)
 	if n == 1 {
-		return n
+		return 1
 	}
 	// recurse = minimum amount of work
-	return 1 + count(n-1)
+	return n + count(n-1)
 }
