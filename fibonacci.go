c381cbdfba44486dea79fd99b286b7e693ba0f35
Aditya Pathania <getaddi1711@gmail.com>
2022-08-18 07:07:13 +0530

    fibonacci

---
+++ fibonacci.go
@@ -0,0 +1,9 @@
+package main
+
+func fibo(n int) int {
+	if n == 0 || n == 1 {
+		return n
+	} else {
+		return fibo(n-1) + fibo(n-2)
+	}
+}
--- main.go
+++ main.go
@@ -3,11 +3,17 @@ package main
 import "fmt"
 
 func main() {
+	// sum of n numbers
 	fmt.Println(count(5))
+	// reverse string
 	fmt.Println(reversestring("hello"))
+	// is palindrome?
 	fmt.Println(palindrome("racecar"))
+	// decimal to binary
 	fmt.Println(decimaltobinary(233, ""))
-
+	// binary search an array
 	nums := []int{2, 5, 7, 11}
 	fmt.Println(binarysearch(nums, 12, 0, len(nums)))
+	//fibonacci
+	fmt.Println(fibo(10))
 }
